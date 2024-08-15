0c7d229512d1b0f82b00ec605276497325aeb476
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-15 16:59:58 -0300

    Projeto- Processo Seletivo

---
+++ candidatura/src/ProcessoSeletivo.java
@@ -0,0 +1,20 @@
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+    }
+
+     static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+
+        if(salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO!");
+        } else if (salarioBase==salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        } else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+     }
+
+}
